0dbf6a831278045507198822704c9bb3d807145c
Iryna Prus <prusirina12@gmail.com>
2025-04-05 18:30:21 +0300

    update.task-1-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function getElementWidth(content, padding, border) {
   content = Number.parseFloat(content);
   padding = Number.parseFloat(padding);
   border = Number.parseFloat(border);
-  return (value = content + padding * 2 + border * 2);
+  return content + padding * 2 + border * 2;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
